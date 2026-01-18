5e0a118c8507645668fefdc2df9761d8c8b892a5
cazcullimore <cazcullimore@gmail.com>
2026-01-17 18:09:27 -0800

    update tests

--- autodiff.py
+++ autodiff.py
@@ -2,6 +2,8 @@ import numpy as np
 import numpy.typing as npt
 from typing import Union, Sequence
 from tensorflow.keras.datasets import mnist
+import sys
+import matplotlib.pyplot as plt
 
 class autodiffArray:
     def __init__(self, value):
@@ -11,18 +13,12 @@ class autodiffArray:
             self.parent2 = None
             self.parentOperator = None
             self.value = value.value
-        # elif isinstance(value, (int, float)):
-            
-        #     self.parent1 = None
-        #     self.parent2 = None
-        #     self.parentOperator = None
-        #     self.value = np.array([value])
         else:
             self.parent1 = None
             self.parent2 = None
             self.parentOperator = None
             self.value = value
-        self.grad = 0.0# np.zeros_like(self.value,  dtype='float64')  # Initialize gradient to zero
+        self.grad = 0.0 # Initialize gradient to zero
 
     def __add__(self, other):
         if not isinstance(other, autodiffArray):
@@ -113,44 +109,46 @@ class autodiffArray:
         elif self.parentOperator == '/':
             self.parent1.grad += self.grad / self.parent2.value
             self.parent2.grad -= self.grad / self.parent2.value ** 2 * self.parent1.value
-        elif self.parentOperator == '@':
-            print("backward matmul")
-            
-            i = [0]
-            def tryit(func):
-                i[0] += 1
-                try:
-                    print(i[0], func())
-                except:
-                    pass
-            tryit(lambda: self.grad.shape)
-            tryit(lambda: self.parent1.value.shape)
-            tryit(lambda: self.parent2.value.shape)
-            tryit(lambda: self.parent1.grad.shape)
-            tryit(lambda: self.parent2.grad.shape)
-            tryit(lambda: self.grad)
-            tryit(lambda: self.parent1.value)
-            tryit(lambda: self.parent2.value)
-            tryit(lambda: self.parent1.grad)
-            tryit(lambda: self.parent2.grad)
-            
+        elif self.parentOperator == '@':            
             self.parent1.grad += self.grad @ self.parent2.value.T
             self.parent2.grad += (self.grad.T @ self.parent1.value).T
-
+        elif self.parentOperator == 'relu':
+            # ReLU derivative is 1 for positive values, 0 for negative values
+            relu_grad = np.where(self.parent1.value > 0, 1, 0)
+            self.parent1.grad += self.grad * relu_grad # parent one and two will always be the same for relu
         self.parent1.backward_helper()
         if self.parent1 != self.parent2: # double counts if they are the same (grad was already twice as much as it should be)
             self.parent2.backward_helper()
+    def zero_grad(self):
+        self.grad = 0.0
+        if isinstance(self.parent1, autodiffArray):
+            self.parent1.zero_grad()
+        if isinstance(self.parent2, autodiffArray):
+            self.parent2.zero_grad()
+
+
+def relu(activation):
+    """
+    Applies the ReLU activation function element-wise.
+    """
+    out = autodiffArray(np.maximum(0, activation.value))
+    out.parent1 = activation
+    out.parent2 = activation
+    out.parentOperator = 'relu'
+    return out
             
 class NeuralNetwork:
     def __init__(self, layer_sizes):
         self.layers = []
         self.biases = []
         for i in range(len(layer_sizes)-1):
-            self.layers.append(autodiffArray(np.random.randn(layer_sizes[i], layer_sizes[i+1])))
+            self.layers.append(autodiffArray(np.random.randn(layer_sizes[i], layer_sizes[i+1]) / np.sqrt(layer_sizes[i] / 2)))
             self.biases.append(autodiffArray(np.random.randn(layer_sizes[i+1])))
     def forward(self, x):
-        for layer, bias in zip(self.layers, self.biases):
-            x = x @ layer + bias.value
+        for i, (layer, bias) in enumerate(zip(self.layers, self.biases)):
+            x = x @ layer + bias
+            if i < len(self.layers) - 1:
+                x = relu(x)
         return x
 
     def zero_grad(self):
@@ -158,19 +156,13 @@ class NeuralNetwork:
             layer.grad = 0
         for bias in self.biases:
             bias.grad = 0
+        
 
     def update(self, learning_rate=0.01):
         for i in range(len(self.layers)):
             self.layers[i].value -= learning_rate * self.layers[i].grad
-            self.biases[i].value -= learning_rate * self.biases[i].grad
-            self.zero_grad()
-        
-
-
-# def autodiffSum(arr: autodiffArray):
-#     arr.value = np.sum(arr.value)
-#     arr.grad = np.on
-
+            self.biases[i].value -= learning_rate * np.average(self.biases[i].grad, axis=0) if isinstance(self.biases[i].grad, np.ndarray) else learning_rate * self.biases[i].grad
+        self.zero_grad()
 
 def test1():
     a = autodiffArray(np.array([2.0]))
@@ -297,7 +289,6 @@ def test_nested_operations_reassign():
     assert np.allclose(a.grad, np.array([0]))
     assert np.allclose(b.grad, np.array([0]))
 
-
 def test_matmul():
     a = autodiffArray(np.array([[1.0, 2.0], [3.0, 4.0]]))
     b = autodiffArray(np.array([[5.0, 6.0], [7.0, 8.0]]))
@@ -316,28 +307,58 @@ def load_mnist(normalize=True, flatten=True):
         x_test = x_test.reshape(-1, 28*28)
     return (x_train, y_train), (x_test, y_test)
 
+def getAcc(y_true, y_pred):
+    return np.mean(np.argmax(y_true, axis=1) == np.argmax(y_pred, axis=1))
+
+def getMinibatch(x, y, batch_size=100):
+    indices = np.random.choice(x.shape[0], batch_size, replace=False)
+    x_batch = x[indices]
+    y_batch = y[indices]
+    return autodiffArray(x_batch), autodiffArray(y_batch)
+
+def avg(arr: autodiffArray):
+    sumAlongRows = autodiffArray(np.ones([1, arr.value.shape[0]])) @ arr
+    return (sumAlongRows @ autodiffArray(np.ones([arr.value.shape[1], 1]))) / (arr.value.shape[0] * arr.value.shape[1])
+
 def testTrainMnist():
     (x_train, y_train), (x_test, y_test) = load_mnist()
-    x_train = autodiffArray(x_train)
-    y_train = autodiffArray(np.eye(10)[y_train])
+    x_test = autodiffArray(x_test)
+    y_test = autodiffArray(np.eye(10)[y_test]) # Convert labels to one-hot
+    x_train = x_train
+    y_train = np.eye(10)[y_train]
     nn = NeuralNetwork([784, 128, 10])  # Example neural network with input size 784 (28x28), hidden layer of size 128, and output layer of size 10
     print("MNIST data loaded and converted to autodiffArray.")
-    for i in range(10):
-        x = x_train
-        y = y_train
+    test_losses = []
+    for i in range(100000):
+        x, y = getMinibatch(x_train, y_train, batch_size=128)
         y_hat = nn.forward(x)
         # Here you can add more operations and test gradients
         # For example, you can compute a loss and call backward
-        sumAlongRows = autodiffArray(np.ones([1, y.value.shape[0]])) @ ((y_hat - y) ** 2)
-        # sumAlongRows.value = sumAlongRows.value[None, :]
-        print("sum shapes",sumAlongRows.value.shape)
-        loss = (sumAlongRows @ autodiffArray(np.ones([y.value.shape[1], 1]))) / (60000 * 100)
-        # loss2 =  (((y_hat - y) ** 2) @ autodiffArray(np.ones([y.value.shape[0]]))) @ autodiffArray(np.ones([y.value.shape[1]]        print(loss2.value, loss.value)
-        print(loss.value.shape)
+        
+        loss = avg((y_hat - y) ** 2)
+       
+        
         loss.backward()
-        nn.update()
-        # print(f"Gradients: x.grad={x.grad}, y.grad={y.grad}")
-        print("epoch", i,"loss:",loss.value)
+        
+        if i % 1000 == 0:
+            print("train acc:", getAcc(y.value, y_hat.value))
+            print("epoch", i,"loss:",loss.value)
+       
+        nn.update(learning_rate=0.01)
+        if i % 10 == 0:
+            y_test_hat = nn.forward(x_test)
+            test_loss = avg((y_test_hat - y_test) **2) 
+            # print(f"Epoch {i}, Test Loss: {test_loss.value[0]}")
+            test_losses.append(test_loss.value[0])
+        nn.zero_grad() # paranoia
+    
+    plt.plot(test_losses)
+    plt.xlabel('Epochs')
+    plt.ylabel('Test Loss')
+    plt.title('Test Loss Over Time')
+    plt.show()
+        
+    
 if __name__ == "__main__":
     test1()
     test_addition_backward_fixed()
